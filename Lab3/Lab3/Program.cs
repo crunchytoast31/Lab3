e791641459ae546dde6c2835249fcf79f2e6e163
Wail Alhashemi <72526651+crunchytoast31@users.noreply.github.com>
2022-09-28 17:01:06 -0400

    Add files via upload

---
+++ Lab3/Lab3/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            bool goOn = false;
+
+
+            Console.WriteLine("What number would you like to learn how to cube and square first?");
+            double num = double.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                double square = i * i;
+                double cube = i * i * i;
+                Console.WriteLine("The number you chose was: " + i + " that number squared is: " + square + " and cubed is: " + cube);
+
+
+                {
+                    while (goOn == true) ;
+                    Console.WriteLine("Would you like to input another number y/n?");
+                    string input = Console.ReadLine();
+                    {
+
+                        if (input == "y") ;
+                        goOn = true;
+
+
+                        if (input == "n") ; goOn = false;
+                    }
+                }
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+    }
+}
